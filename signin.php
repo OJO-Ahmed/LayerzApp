f9ea161b630593e2c2c13edf7afc860a5319366a
OJO-Ahmed <adewunmigenius@gmail.com>
2017-07-18 17:16:54 +0100

    php file for processing, when each user sign in

---
+++ signin.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'socialdb.php';
+
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+$sql ="SELECT*FROM social WHERE uid='$uid' AND pwd ='$pwd'";
+$result = $conn->query($sql);
+ if(!$row = $result->fetch_assoc()){
+ 	echo "Your username or password is incorrect!";
+ } else {
+ 	$_SESSION['user_data'] = $row;
+ 	$link = 'user.php?id=' . $row["id"] . '';
+ 	header("Location: " . $link);
+ }
+
+// header("Location: index.php");
