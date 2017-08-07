9d71f034cee5178e0e44480c80572f096f6342f1
OJO-Ahmed <adewunmigenius@gmail.com>
2017-08-07 13:44:15 +0100

    update signup php script

--- signup.php
+++ signup.php
@@ -1,14 +1,21 @@
 <?php
-session_start();
-include 'socialdb.php';
+if(!isset($_SESSION)){
+	session_start();
+}
+
+require_once 'socialdb.php';
 
 $first = $_POST['first'];
 $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
+$salt1 = "qm&h*";
+$salt2 = "pg!@";
+$prtpwd = hash('ripemd128', "$salt1$pwd$salt2");
+
 $sql ="INSERT INTO social(first,last,uid,pwd)
-VALUES ('$first', '$last', '$uid', '$pwd')";
+VALUES ('$first', '$last', '$uid', '$prtpwd')";
 $result = $conn->query($sql);
 
 header("location: index.php");
