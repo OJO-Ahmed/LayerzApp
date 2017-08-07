af058c19aad31ce62a43ade37642eca97b010f16
OJO-Ahmed <adewunmigenius@gmail.com>
2017-08-07 13:43:19 +0100

    index page

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
+if(!isset($_SESSION)){
+	session_start();
+}
 	require_once 'socialdb.php';
-	if ($_SESSION['user_data'] && $_SESSION['user_data']['id']) {
+
+	if(isset($_COOKIE['uid']) && isset($_COOKIE['pwd'])){
+	header('Location: user.php?id='.$_SESSION['user_data']['id'].'');
+	}
+	elseif ($_SESSION['user_data'] && $_SESSION['user_data']['id']) {
 		header('Location: user.php?id='.$_SESSION['user_data']['id'].'');
 	}
 ?>
@@ -17,31 +24,38 @@
 		<img id ="logo" src="pix/layerz.jpg" alt="logo">
 		<h2><span id="meet">Meet</span> wonderful <span id="people">people</span></h2>
 	</div>
+	<section id="signin">
+			<form action="signin.php" method="POST">
+			<input type="text" id="username" placeholder="username" name="uid"/>
+			<input type="password" id="pwd" placeholder="password"  name="pwd"/>
+			<input type="submit" value="Sign in" />
+			</form>
+	</section>
 </header>
 <div id="homewrapper">
 	<div id="homemain">
 		<section >
 			<img id="image" src="pix/homepix.png" alt = "picture"/>
 		</section>
-		<section id="signin">
-			<div class = "form">
-				<form action="signin.php" method="POST">
-				<input type="text" id="username" placeholder="username" name="uid"/><br/>
-				<input type="password" id="pwd" placeholder="password"  name="pwd"/><br/>
-				<input type="submit" value="Sign in" />
-				</form>
-		</section>
 		<section id="Signup">
 			<div class = "form">
 				<form action="signup.php" method="POST">
+				<p>
 				<label for="firstname">firstname</label>
-				<input type="text" id="firstname" placeholder="firsname" name="first" /><br/>
+				<input type="text" id="firstname" name="first" /><br/>
+				</p>
+				<p>
 				<label for="lastname">lastname</label>
-				<input type="text" id="lastname" placeholder="lastname" name="last"/><br/>
+				<input type="text" id="lastname" name="last"/><br/>
+				</p>
+				<p>
 				<label for="username">username</label>
-				<input type="text" id="username" placeholder="username" name="uid"/><br/>
+				<input type="text" id="username" name="uid"/><br/>
+				</p>
+				<p>
 				<label for="pwd">password</label>
-				<input type="password" id="pwd"  name="pwd"/><br/>
+				<input type="password" id="pwd" name="pwd"/><br/>
+				</p>
 				<input type="submit" value="Sign up" />
 				</form>
 			</div>
